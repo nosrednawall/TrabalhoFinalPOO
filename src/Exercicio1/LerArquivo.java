1554da58da58f8775edffe0392ea742792483b9d
Anderson <Anderson@localhost>
2017-06-12 00:09:37 -0300

    Enviando modificacoes para efetuar testes com o notebook

--- src/Exercicio1/LerArquivo.java
+++ src/Exercicio1/LerArquivo.java
@@ -1,53 +1,63 @@
 package Exercicio1;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
 
-public class LerArquivo implements java.io.Serializable {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3343194996666033488L;
+public class LerArquivo {
+
 	static String texto;
-	
-	public LerArquivo() {
-		System.out.println("Entrou no construtor");
-		File arquivo = new File("arq.txt");
 
+	public static void LendoArquivo(String arquivo) {
 		try {
-			FileReader ler = new FileReader(arquivo);
+			FileReader ler = new FileReader(new File(arquivo));
 			BufferedReader leitor = new BufferedReader(ler);
-			System.out.println("Entrou no try");
-			
 			String texto = leitor.readLine();
-
-			while (texto != null) {
-				System.out.printf("%s\n", texto);
-				texto=leitor.readLine();	
+			while (true) {
+				if (texto != null) {
+					//System.out.printf("%s\n", texto);
+				} else {
+					break;
+				}
+				texto = leitor.readLine();
 			}
-			System.out.println("Terminou com sucesso o while");
 			ler.close();
-				
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	
 	}
-
+	
+	public static void EnviandoArquivo(int porta){
+		try {
+			@SuppressWarnings("resource")
+			ServerSocket servidor = new ServerSocket(porta);
+			Socket conexao = servidor.accept();
+			DataOutputStream saida = new DataOutputStream(conexao.getOutputStream());
+			SocketAddress s = conexao.getRemoteSocketAddress();
+			System.out.println("Passou");
+			System.out.println("Enviando arquivo ao cliente"+s.toString());
+			saida.writeUTF(texto);
+			saida.flush();
+			saida.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
-/*		int porta = 12258;
-		ServerSocket servidor;
- * 				
+/*
+ * int porta = 12258; ServerSocket servidor;
  * 
  * 
- * 				servidor = new ServerSocket(porta, 2);
-				Socket conexao = servidor.accept();
-				DataOutputStream saida = new DataOutputStream(conexao.getOutputStream());
-				SocketAddress s = conexao.getRemoteSocketAddress();
+ * 
+ * servidor = new ServerSocket(porta, 2); Socket conexao = servidor.accept();
+ * DataOutputStream saida = new DataOutputStream(conexao.getOutputStream());
+ * SocketAddress s = conexao.getRemoteSocketAddress();
  */
--- src/Exercicio1/PrincipalExercicioArquivo.java
+++ src/Exercicio1/PrincipalExercicioArquivo.java
@@ -3,8 +3,9 @@ package Exercicio1;
 public class PrincipalExercicioArquivo {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		LerArquivo ler = new LerArquivo();
+		String arquivo = "/home/Anderson/workspace/TrabalhoFinal/arq.txt";
+		int porta = 12258;
+		LerArquivo.LendoArquivo(arquivo);
+		LerArquivo.EnviandoArquivo(porta);
 	}
-
 }
