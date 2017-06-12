fbe993df7156312ca75fb858c1ed12c8555f9e2e
Anderson <Anderson@localhost>
2017-06-11 23:13:17 -0300

    Leitura de arquivos efetuada com sucesso

--- src/Exercicio1/LerArquivo.java
+++ src/Exercicio1/LerArquivo.java
@@ -1,41 +1,53 @@
 package Exercicio1;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.SocketAddress;
 
-public class LerArquivo {
+public class LerArquivo implements java.io.Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3343194996666033488L;
 	static String texto;
 	
 	public LerArquivo() {
-		
+		System.out.println("Entrou no construtor");
 		File arquivo = new File("arq.txt");
-		FabricaConexoes conexao = new FabricaConexoes();
-		
+
 		try {
 			FileReader ler = new FileReader(arquivo);
 			BufferedReader leitor = new BufferedReader(ler);
-			while (leitor.ready()) {
-				//System.out.println(leitor.readLine());
-				texto=leitor.readLine();
-				conexao.CriarConexao();
-				DataOutputStream saida = new DataOutputStream(conexao.getOutputStream());
-				SocketAddress s = conexao.getRemoteSocketAddress();
+			System.out.println("Entrou no try");
+			
+			String texto = leitor.readLine();
+
+			while (texto != null) {
+				System.out.printf("%s\n", texto);
+				texto=leitor.readLine();	
 			}
+			System.out.println("Terminou com sucesso o while");
+			ler.close();
 				
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	
 	}
 
 }
+/*		int porta = 12258;
+		ServerSocket servidor;
+ * 				
+ * 
+ * 
+ * 				servidor = new ServerSocket(porta, 2);
+				Socket conexao = servidor.accept();
+				DataOutputStream saida = new DataOutputStream(conexao.getOutputStream());
+				SocketAddress s = conexao.getRemoteSocketAddress();
+ */
--- src/Exercicio1/PrincipalExercicioArquivo.java
+++ src/Exercicio1/PrincipalExercicioArquivo.java
@@ -3,8 +3,8 @@ package Exercicio1;
 public class PrincipalExercicioArquivo {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("unused")
 		LerArquivo ler = new LerArquivo();
-
 	}
 
 }
